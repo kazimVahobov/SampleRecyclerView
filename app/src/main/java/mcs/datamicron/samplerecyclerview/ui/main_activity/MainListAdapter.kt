0533648111fbf38cb31168243779f38ac0dde1c1
double_bUz <penzakokand@gmail.com>
2020-07-15 13:46:14 +0500

    implement animation for items of list

--- app/src/main/java/mcs/datamicron/samplerecyclerview/ui/main_activity/MainListAdapter.kt
+++ app/src/main/java/mcs/datamicron/samplerecyclerview/ui/main_activity/MainListAdapter.kt
@@ -2,12 +2,14 @@ package mcs.datamicron.samplerecyclerview.ui.main_activity
 
 import android.content.Context
 import android.text.format.DateFormat
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
@@ -32,12 +34,16 @@ class MainListAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = list[position]
+        holder.relativeLayout.animation = AnimationUtils.loadAnimation(context, R.anim.fade_scale)
+
         holder.title.text = item.title
         holder.content.text = item.content
         val cal: Calendar = Calendar.getInstance(Locale.ENGLISH)
         cal.timeInMillis = item.time
         val date: String = DateFormat.format("dd-MM-yyyy", cal).toString()
         holder.time.text = date
+        holder.avatarCardView.animation =
+            AnimationUtils.loadAnimation(context, R.anim.fade_transition)
         Glide.with(context).load(item.avatar).transform(CircleCrop())
             .into(holder.avatar)
     }
@@ -47,6 +53,7 @@ class MainListAdapter(
         var content: TextView = itemView.findViewById(R.id.contentTV)
         var time: TextView = itemView.findViewById(R.id.dateTV)
         var avatar: ImageView = itemView.findViewById(R.id.avatar)
-
+        var avatarCardView: CardView = itemView.findViewById(R.id.avatarCardView)
+        var relativeLayout: RelativeLayout = itemView.findViewById(R.id.relativeLayout)
     }
 }
